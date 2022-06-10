d965ad213fa27d1c810bbc89c6b48ed49915b9e4
nasiegel88 <nasiegel@ucdavis.edu>
2022-06-10 14:55:30 -0700

    switching to docker

---
+++ install.R
@@ -0,0 +1,3 @@
+devtools::install_github("CharlieCarpenter/tidyMicro@0e6f7b5", build_vignettes = FALSE, upgrade = "never") 
+devtools::install_github("jbisanz/qiime2R@2a3cee1", upgrade = "never")
+devtools::install_github("ChiLiubio/microeco@e9e8bcd", upgrade = "never")

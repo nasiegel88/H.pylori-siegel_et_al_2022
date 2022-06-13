f857643f58c7a353e5faf9f9239290eff522ee1e
nasiegel88 <nasiegel@ucdavis.edu>
2022-06-13 16:34:54 -0700

    move to conda

--- install.R
+++
@@ -1,3 +0,0 @@
-devtools::install_github("CharlieCarpenter/tidyMicro@0e6f7b5", build_vignettes = FALSE, upgrade = "never") 
-devtools::install_github("jbisanz/qiime2R@2a3cee1", upgrade = "never")
-devtools::install_github("ChiLiubio/microeco@e9e8bcd", upgrade = "never")
